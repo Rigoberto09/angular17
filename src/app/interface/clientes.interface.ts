a1a27e4052205fd5b45f4cc3495253eef4639524
Rigoberto09 <rigobertoborjas3@gmail.com>
2024-02-08 13:36:43 -0600

    Desarrollo de nueva interface

--- src/app/interface/clientes.interface.ts
+++ src/app/interface/clientes.interface.ts
@@ -12,3 +12,15 @@ export interface Cliente {
     usuario_borro: string | null;
   }
   // export interface cadenas:string;
+  export interface ClienteInset {
+    primer_nombre: string;
+    segundo_nombre: string;
+    primer_apellido: string;
+    segundo_apellido: string;
+    direccion: string;
+    telefono: string;
+    correo: string;
+    fecha_registro: string;
+    usuario_creo: string;
+    usuario_borro: string | null;
+  }
