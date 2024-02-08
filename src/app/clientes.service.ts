bfb158b674dae0af77362dcb6baad60a14277f42
Rigoberto09 <rigobertoborjas3@gmail.com>
2024-02-08 16:47:01 -0600

    Desarollo de funciones de actualizar y eliminar

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -4,20 +4,19 @@ import { Observable, catchError, of } from 'rxjs';
 import { Cliente, ClienteInset } from './interface/clientes.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClientesService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  urll:string="https://localhost:7000/cliente/listar";
-  urlApi2017:string="http://localhost:64051/api/clientes";
+  urll: string = 'https://localhost:7000/cliente/listar';
+  urlApi2017: string = 'http://localhost:64051/api/clientes';
   // url:string="https://localhost:44391/api/clientes";
- 
+
   getClientes(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(this.urlApi2017);
   }
-   // getClientes():Observable<Cliente | undefined>{
+  // getClientes():Observable<Cliente | undefined>{
   //  return this.http.get<Cliente>(this.url).pipe(
   //   catchError((e)=>{
   //     console.log(e);
@@ -28,14 +27,19 @@ export class ClientesService {
 
   //agregar datos a la base de datos
   postCliente(cliente: ClienteInset): Observable<ClienteInset> {
-    console.log("valres a insertar",cliente);
-    return this.http.post<Cliente>(this.urlApi2017,cliente);
-
+    console.log('valres a insertar', cliente);
+    return this.http.post<Cliente>(this.urlApi2017, cliente);
   }
 
   //eliminar cliente
   deleteCliente(id: number): Observable<any> {
     const url = `${this.urlApi2017}/${id}`; // Concatena el ID a la URL de la API
     return this.http.delete<any>(url);
+  }
+  //actualizar clientes
+  actualizar(id: number, datosActualizados: ClienteInset): Observable<any> {
+    const url = `${this.urlApi2017}/${id}`;
+    console.log("datos actualizar",datosActualizados);
+    return this.http.put<any>(url, datosActualizados);
   }
 }
